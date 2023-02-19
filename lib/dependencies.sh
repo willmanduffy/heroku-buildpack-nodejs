eabf66c2bf9bc2cd74772a06df7ace35cea87c03
Will Duffy <willmanduffy@gmail.com>
2023-02-19 17:15:34 -0500

    Once more

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -178,7 +178,7 @@ yarn_prune_devdependencies() {
     meta_set "skipped-prune" "false"
   else
     cd "$build_dir" || return
-    monitor "yarn-prune" yarn install --frozen-lockfile --ignore-engines --ignore-scripts --prefer-offline 2>&1
+    monitor "yarn-prune" yarn install --frozen-lockfile --ignore-scripts --prefer-offline 2>&1
     meta_set "skipped-prune" "false"
   fi
 }
